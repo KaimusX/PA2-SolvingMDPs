7b87dfbe6ffe108a590a08938bb3a9dcaa1f2fc6
diegodeleon-ai <ddeleo18@my.epcc.edu>
2026-03-27 09:55:33 -0600

    Add value_iteration function for Part 1
    Implement value iteration algorithm for policy determination.

--- part1.py
+++ part1.py
@@ -17,3 +17,34 @@ actions = {
     "TD10a": ["P", "R", "S"],
     "11aFin": []
 }
+
+
+
+#Part 1: Value Iteration
+def value_iteration(states, actions, transition_model, reward_function, gamma, epsilon):
+        V = {s: 0 for s in states} ##every state has est. value of 0
+        
+        while True:
+            delta = 0
+            for s in states:
+                v = V[s]
+                #Bellman Equation applied to every state
+                #for each action a, computes expected value from
+                #taking that action in state s. Picks max Value
+                V[s] = max(sum(transition_model(s,a,s_next) * 
+                    (reward_function(s,a,s_next)+ gamma *
+                        V[s_next])for s_next in states) for a in actions)
+                delta = max(delta, abs(v - V[s])) #keeps track of biggest change seen this iteration
+                if delta < epsilon:
+                    break
+                #Conversion:loop stops once no state's value changes by more than epsilon
+                #For current state (s), find best possible action
+            policy = {}
+            for s in states:
+                policy[s] = max(actions,
+                                    key = lambda a: sum(
+                                        transition_model(s,a,s_next *
+                                            reward_function(s,a,s_next) + gamma *
+                                            V[s_next])
+                                    for s_next in states))
+                return policy, V
