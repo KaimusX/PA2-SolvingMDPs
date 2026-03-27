fb02dce10dfbe27bafd97492022d86a72c88b31b
Luis Franco <luisfranco.business@gmail.com>
2026-03-27 09:37:28 -0600

    Added states and possible actions

---
+++ part1.py
@@ -0,0 +1,19 @@
+# Defining MDP
+
+# Possible states
+states = ["RU8p", "TU10p", "RU10p", "RD10p", "RU8a", "RD8a", "TU10a", "RU10a", "RD10a", "TD10a", "11aFin"]
+
+# Possible actions from each state.
+actions = {
+    "RU8p": ["P", "R", "S"],
+    "TU10p": ["P", "R"],
+    "RU10p": ["R", "P", "S"],
+    "RD10p": ["R", "P"],
+    "RU8a": ["P", "R", "S"],
+    "RD8a": ["R", "P"],
+    "TU10a": ["P", "R", "S"],
+    "RU10a": ["P", "R", "S"],
+    "RD10a": ["P", "R", "S"],
+    "TD10a": ["P", "R", "S"],
+    "11aFin": []
+}
